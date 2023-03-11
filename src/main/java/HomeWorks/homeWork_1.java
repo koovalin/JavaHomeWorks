1da7fb3f4f64462bef5555582f7adc693d75bf6f
koovalin <koovalin@gmail.com>
2023-03-11 13:10:19 +0300

    Add Task_3

--- src/main/java/HomeWorks/homeWork_1.java
+++ src/main/java/HomeWorks/homeWork_1.java
@@ -1,25 +1,33 @@
 package HomeWorks;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class homeWork_1 {
     public static void main(String[] args) {
-        Scanner reader = new Scanner(System.in);
-        System.out.println("Введите год: ");
-        int year = Integer.parseInt(reader.next());
-        if (isLeapYear(year)) {
-            System.out.println("Год весокосный!");
-        }
-        else{
-            System.out.println("Год не весокосный!");
-        }
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Введи число: ");
+        int numToSort = Integer.parseInt(scan.next());
+        int[] myArray = new int[]{3, 4, 6, 7, 5, 2, 1, 5, 4, 3, 2, 1, 4, 5, 4};
+        System.out.println("Array: " + Arrays.toString(myArray));
+        System.out.println("Sorted array: " + Arrays.toString(arraySort(myArray, numToSort)));
     }
 
-    public static boolean isLeapYear(int year) {
-        if (year % 4 != 0) {
-            return false;
-        } else if (year % 400 == 0) {
-            return true;
-        } else return year % 100 != 0;
+    private static int[] arraySort(int[] arr, int numToSort) {
+        int len = arr.length;
+        int[] intArray = new int[len];
+        int i = 0;
+        for(int number = 0; number < len; number++) {
+            if (arr[number] != numToSort) {
+                intArray[i] = arr[number];
+                i++;
+            }
+            if (number + 1 == len) {
+                for (int j = i; j < len; j++) {
+                    intArray[j] = numToSort;
+                }
+            }
+        }
+        return intArray;
     }
 }
