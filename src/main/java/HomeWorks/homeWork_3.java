c2c52c5e71b5731c6b5478230900f74b8be4c083
koovalin <koovalin@gmail.com>
2023-03-15 23:05:33 +0300

    Add HW_Task_1

--- src/main/java/HomeWorks/homeWork_3.java
+++ src/main/java/HomeWorks/homeWork_3.java
@@ -10,17 +10,8 @@ public class homeWork_3 {
         for (int i = 0; i < rand.nextInt(10, 20); i++) {
             intArr.add(rand.nextInt(10));
         }
-//        System.out.printf("Integer Array => %s", intArr);
-//        intArr.removeIf(e -> (e % 2) == 0);
-//        System.out.printf("\nEven deleted Array => %s", intArr);
-
-//        2. Задан целочисленный список ArrayList. Найти минимальное, максимальное и среднее арифметическое из этого списка.
-        int max = Collections.max(intArr);
-        int min = Collections.min(intArr);
-        float avg = ((float) min + (float) max) / 2;
-
-        System.out.printf("\nMax in Array => %s", max);
-        System.out.printf("\nMin in Array => %s", min);
-        System.out.printf("\nAvg between Min & Max => %s", avg);
+        System.out.printf("Integer Array => %s", intArr);
+        intArr.removeIf(e -> (e % 2) == 0);
+        System.out.printf("\nEven deleted Array => %s", intArr);
     }
 }
