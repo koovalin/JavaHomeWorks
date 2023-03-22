862ba1ea659857d558afe6c716eaa692615743d9
koovalin <koovalin@gmail.com>
2023-03-22 23:27:32 +0300

    Add HW_6_Task_2

--- src/main/java/HomeWorks/homeWork_6.java
+++ src/main/java/HomeWorks/homeWork_6.java
@@ -1,8 +1,19 @@
 package HomeWorks;
 
+import java.util.*;
+
 public class homeWork_6 {
     public static void main(String[] args) {
-        Laptop newNote_MSI = new Laptop(1, "MSI", "Linux", 512, 16, "Black");
-        newNote_MSI.displayInfo();
+        Laptop laptopMSI = new Laptop(1, "MSI", "Windows", 512, 16, "Black");
+        Laptop laptopAcer = new Laptop(2, "Acer", "Windows", 1024, 16, "White");
+        Laptop laptopHP = new Laptop(3, "HP", "Windows", 512, 8, "Grey");
+        Laptop laptopApple = new Laptop(4, "Apple", "macOS", 1024, 16, "Grey");
+
+        List<Laptop> laptopList = new ArrayList<>();
+        laptopList.add(laptopMSI);
+        laptopList.add(laptopAcer);
+        laptopList.add(laptopHP);
+        laptopList.add(laptopApple);
+
     }
 }
